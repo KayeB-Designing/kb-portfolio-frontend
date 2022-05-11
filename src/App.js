cbc6f5b5a5d186a4c5d36741aa82d97930b1affa
Kaye Bryant <bryantk72391@gmail.com>
2022-05-11 15:18:43 -0400

    app.js set up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+// IMPORT COMPONENTS
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Route, Routes } from "react-router-dom";
+// IMPORT PAGES
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      const URL = "https://kb-portfolio-lab.herokuapp.com/";
+      <Header />
+      <Routes>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/projects">
+          <Projects URL={"https://kb-portfolio-lab.herokuapp.com/projects/"} />
+        </Route>
+        <Route path="/about">
+          <About URL={"https://kb-portfolio-lab.herokuapp.com/about/"} />
+        </Route>
+      </Routes>
+      <Footer />
     </div>
   );
 }
